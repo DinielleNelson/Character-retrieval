50c79ce9cbaaffb4ccad62715f0b8de31b8d4e8e
Dinielle <Dinielle.Nelson@gmail.com>
2019-03-17 14:51:17 -0400

    completed 3 thru 7a. 7b thru 8 remain

--- Homework script - DYN.sql
+++ Homework script - DYN.sql
@@ -23,39 +23,81 @@ ORDER BY last_name, first_name;
 SELECT country_id, country FROM country
 WHERE country IN ('Afghanistan', 'Bangladesh', 'China');
 
+-- * 3a. create a column in the table `actor` named `description` and use the data type `BLOB` 
+ALTER TABLE actor
+ADD COLUMN description BLOB;
 
+-- * 3b. Delete the `description` column.
+ALTER TABLE actor
+DROP COLUMN description;
+
+-- * 4a. List the last names of actors, as well as how many actors have that last name.
+SELECT last_name, COUNT(*) FROM actor
+GROUP BY last_name;
+
+-- * 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+SELECT last_name, COUNT(*) FROM actor
+GROUP BY last_name
+HAVING COUNT(*) >=2;
+
+-- * 4c. The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`. Write a query to fix the record.
+UPDATE actor
+SET first_name='HARPO'
+WHERE first_name='GROUCHO' AND last_name='WILLIAMS';
+
+-- * 4d. In a single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`.
+UPDATE actor
+SET first_name='GROUCHO'
+WHERE first_name='HARPO' AND last_name='WILLIAMS';
+
+-- * 5a. You cannot locate the schema of the `address` table. Which query would you use to re-create it?
+SHOW CREATE TABLE address;
+
+-- * 6a. Use `JOIN` to display the first and last names, as well as the address, of each staff member. Use the tables `staff` and `address`:
+SELECT first_name, last_name, address.address FROM STAFF
+INNER JOIN address
+USING (address_id);
+
+-- * 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005. Use tables `staff` and `payment`.
+SELECT first_name, last_name, SUM(payment.amount) AS 'Total rung up' FROM staff
+INNER JOIN payment
+USING (staff_id)
+GROUP BY staff_id;
+
+-- * 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005. Use tables `staff` and `payment`.
+SELECT first_name, last_name, SUM(payment.amount) AS 'Total rung up' FROM staff
+INNER JOIN payment
+USING (staff_id)
+WHERE YEAR(payment_date) = 2005 AND MONTH(payment_date) = 08
+GROUP BY staff_id;
+
+-- * 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` and `film`. Use inner join.
+SELECT title, COUNT(film_actor.actor_id) AS 'No. of Films' FROM film
+INNER JOIN film_actor
+USING (film_id)
+GROUP BY title;
+
+-- * 6d. How many copies of the film `Hunchback Impossible` exist in the inventory system?
+SELECT COUNT(*) AS 'No. of Copies' FROM inventory
+WHERE film_id IN (
+	SELECT film_id FROM film
+    WHERE title = 'Hunchback Impossible')
+;
+
+-- * 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. List the customers alphabetically by last name.
+SELECT first_name, last_name, SUM(amount) AS 'Total Paid' FROM customer
+INNER JOIN payment USING (customer_id)
+GROUP BY customer_id
+ORDER BY last_name;
+
+-- * 7a. Use subqueries to display the titles of movies starting with the letters `K` and `Q` whose language is English.
+SELECT title FROM film
+WHERE title LIKE 'K%' OR 'Q%' AND language_id IN (
+	SELECT language_id FROM language
+    WHERE name = "English")
+;
 
 /*
--- * 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, so create a column in the table `actor` named `description` and use the data type `BLOB` (Make sure to research the type `BLOB`, as the difference between it and `VARCHAR` are significant).
-
-* 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the `description` column.
-
-* 4a. List the last names of actors, as well as how many actors have that last name.
-
-* 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
-
-* 4c. The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`. Write a query to fix the record.
-
-* 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct name after all! In a single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`.
-
-* 5a. You cannot locate the schema of the `address` table. Which query would you use to re-create it?
-
-  * Hint: [https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html](https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html)
-
-* 6a. Use `JOIN` to display the first and last names, as well as the address, of each staff member. Use the tables `staff` and `address`:
-
-* 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005. Use tables `staff` and `payment`.
-
-* 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` and `film`. Use inner join.
-
-* 6d. How many copies of the film `Hunchback Impossible` exist in the inventory system?
-
-* 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. List the customers alphabetically by last name:
-
-  ![Total amount paid](Images/total_payment.png)
-
-* 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters `K` and `Q` have also soared in popularity. Use subqueries to display the titles of movies starting with the letters `K` and `Q` whose language is English.
-
 * 7b. Use subqueries to display all actors who appear in the film `Alone Trip`.
 
 * 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
